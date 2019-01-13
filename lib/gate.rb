fc2dbfd3749acc8b53928a2082d440cb4f76d033
devzip8 <pi3kp85@gmail.com>
2019-01-13 12:12:48 +0900

    added comments for rdoc (but ignore doc dir)

--- lib/gate.rb
+++ lib/gate.rb
@@ -3,10 +3,19 @@ class Gate
   STATIONS = [:umeda, :juso, :mikuni]
   FARES = [150, 190]
 
+  # Gateオブジェクトの作成
+  # ==== 引数
+  # * +name+ - 駅名
   def initialize(name)
     @name = name
   end
 
+
+  # 入場済みの切符の運賃を計算する
+  # ==== 引数
+  # * +ticket+ - 切符
+  # ==== 戻り値
+  # * +fare+ - 運賃
   def calc_fare(ticket)
     from = STATIONS.index(ticket.stamped_at)
     to = STATIONS.index(@name)
@@ -14,10 +23,20 @@ class Gate
     FARES[distance - 1]
   end
 
+  
+  # 改札機を通って駅に入場する
+  # ==== 引数
+  # * +ticket+ - 切符
   def enter(ticket)
     ticket.stamp(@name)
   end
 
+
+  # 改札機を通って駅から出場する
+  # ==== 引数
+  # * +ticket+ - 切符
+  # ==== 戻り値
+  # * +boolean+ - 運賃が足りていれば+true+、不足していれば+false+
   def exit(ticket)
     fare = calc_fare(ticket)
     fare <= ticket.fare
